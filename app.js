700309e44cb251769bf0b0baff89f35f49960671
Ola Assem <ola.assem@viget.com>
2020-07-12 17:28:20 +0200

    sets up delete item event listener

--- app.js
+++ app.js
@@ -110,7 +110,8 @@ let UIController = (function() {
         budgetLabel: '.budget__value',
         incomeLabel: '.budget__income--value',
         expensesLabel: '.budget__expenses--value',
-        percentageLabel: '.budget__expenses--percentage'
+        percentageLabel: '.budget__expenses--percentage',
+        container: '.container'
     }
 
     return {
@@ -130,13 +131,13 @@ let UIController = (function() {
                 
                 element = DOMStrings.incomeContainer;
 
-                html = '<div class="item clearfix" id="income-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
+                html = '<div class="item clearfix" id="inc-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
 
             } else if (type === 'exp') {
                 
                 element = DOMStrings.expensesContainer;
 
-                html = '<div class="item clearfix" id="expense-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__percentage">21%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
+                html = '<div class="item clearfix" id="exp-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__percentage">21%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
             }
 
             // Replace the placeholder text with actual data recieved from object
@@ -199,7 +200,11 @@ let controller = (function(budgetCtrl, UICtrl) {
 
             }
         });
+
+        document.querySelector(DOM.container).addEventListener('click', ctrlDeleteItem);
     };
+
+
 
     let updateBudget = function(){
 
@@ -232,6 +237,24 @@ let controller = (function(budgetCtrl, UICtrl) {
 
             // 5. Calculate & update budget.
             updateBudget();
+        }
+    };
+
+    let ctrlDeleteItem = function(e){
+        let itemID, splitID, type, ID;
+        
+        itemID = e.target.parentNode.parentNode.parentNode.parentNode.id;
+        
+        if(itemID) {
+            splitID = itemID.split('-');
+            type = splitID[0];
+            ID = splitID[1];
+
+            // 1. Delete item from the data structure.
+
+            // 2. Delete item from the UI.
+
+            // 3. Update and show the new budget.
         }
     };
 
